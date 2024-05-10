e3b2c3a4c17bac52b73a7dc992e62a3a384f9538
fein2 <168700704+fein2@users.noreply.github.com>
2024-05-10 14:18:14 +1000

    added helper functions

--- missionA2S2.m
+++ missionA2S2.m
@@ -21,3 +21,77 @@ title('DC Motor System Step Response Output');
 xlabel('Time (seconds)');
 ylabel('Voltage Output (\Psi)');
 
+%% helper functions
+
+% function definitions in matlab either need to be in their own file,
+% or can be in at the bottom of a script.
+% if you want to these functions outside this lab, feel free to 
+% move them into their own file, just make sure the filename is the same 
+% as the function name, ie timevec.m, freqvec.m
+
+
+function t= timevec(t0, t0_plus_T, n)
+% Creates time vector, where upper limit is non-inclusive
+%          t0 <= t < t0_plus_T
+%   It is the responsibility of the user to ensure that for the use-case
+%   that they want the lower limit included, and the upper-limit 
+%   not included.
+%
+%   Args:
+%   t0 = start time
+%   t0_plus_T = end time (t0 + T)
+%   n = number of samples
+
+    t = linspace(t0, t0_plus_T, n + 1);
+    t = t(1:end - 1);
+end
+
+%% helper functions
+
+function f=freqvec(fs, n)
+% Creates frequency vector suitable for plotting magnitude/phase spectrum
+%
+%  This function emulates the np.fft.freqvec function from python, but will
+%  also make sure that the frequency vector has been shifted correctly, so
+%  that the first index is for the lowest frequency, highest index is for
+%  highest and that the middle frequency is DC.
+
+%  https://numpy.org/doc/stable/reference/generated/numpy.fft.fftfreq.html
+%  
+%  The frequency vector will be slightly different if the sequence is of 
+%  even length or odd length. The specifics of this has to do with 
+%  properties of the Discrete Fourier Transform (DTF), which is what the
+%  FFT algorigthm actually computes. Will likely cover
+%  this in your Digital signal processing class. The main thing to know is 
+%  that this function will create a frequency vector that will ensure the DC
+%  Component is at the correct location. For now, can just take our
+%  word for it, and know what the function does and what it will return.
+%
+% 
+%  For even length signals, our frequency vector will be of the form,
+%        -fs/2 <= f < fs/2
+%  For odd length signals, will be,
+%        -fs/ 2 < f < fs/2
+%
+%  Args:
+%  fs = sample frequency in Hz
+%  n = length of the time vector/number of samples
+
+
+    % if is an even sequence length, generating the frequency vector
+    % is just like doing it for our time vector in the timevec function
+    if mod(n, 2) == 0
+        f_str = sprintf('Generating freq vec\n [%.2f, %.2f)\n', -fs/2, fs/2);
+        disp(f_str);
+        % compute the frequency vector
+        f = linspace(-fs / 2, fs / 2, n + 1);
+        f = f(1:end - 1); 
+    % otherwise is of odd length
+    else        
+        f_str = sprintf('Generating freq vec\n (%.2f, %.2f)\n', ...
+            -(n -1)/2 * fs / n, (n -1)/2 * fs / n);
+        disp(f_str);
+        % compute the frequency vector
+        f = linspace(-(n -1)/ 2, (n -1) / 2, n) * fs / n;
+    end
+end
